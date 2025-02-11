444fb7e120ca21e7c54a9e91bc91a40c20bf1ee1
ryhkml <reyhank95@hotmail.com>
2025-02-11 21:33:15 +0700

    Create MAX_CONNECTIONS comment

--- src/http.h
+++ src/http.h
@@ -4,6 +4,16 @@
 #include <signal.h>
 
 #define BUFFER_SIZE 1024
+// Specify the backlog queue size for the socket.
+//
+// MAX_CONNECTIONS is not a limit on concurrent connections processed.
+// It does not restrict the number of connections that a multi-threaded server can handle simultaneously.
+//
+// MAX_CONNECTIONS only limits the number of connection requests pending in the backlog queue before being accepted by
+// accept(). Once a connection is accepted by accept(), it is handled by a separate thread and is no longer subject to
+// the MAX_CONNECTIONS limit.
+//
+// The optimal value for MAX_CONNECTIONS depends on the estimated server load.
 #define MAX_CONNECTIONS 16
 
 extern volatile sig_atomic_t running;
