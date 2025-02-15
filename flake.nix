9d819e1e606d218761bd7d993f9ebbbf1516794e
ryhkml <reyhank95@hotmail.com>
2025-02-15 14:14:25 +0700

    Add new nix flake

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Throwing HTTP exception in Nginx";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          exception-filter = pkgs.stdenv.mkDerivation {
+            pname = "exception-filter";
+            version = "0.0.0-placeholder";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              gcc
+              gnumake
+            ];
+            buildPhase = ''
+              make
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp out/exception-filter $out/bin/
+            '';
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/exception-filter";
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gcc
+            gnumake
+          ];
+          shellHook = ''
+            echo "Exception Filter development shell"
+            echo "Build dengan: nix build"
+          '';
+        };
+      }
+    );
+}
